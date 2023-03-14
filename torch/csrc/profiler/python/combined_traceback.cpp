845c2d89700283c072ba140d07ecf4aef5fc21de
Zachary DeVito <zdevito@meta.com>
2023-03-13 18:25:53 -0700

    [memory debugging] Extract frame information from inductor
    ghstack-source-id: a9ec72851f67bc1223519b1551203a7a96be1fb9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95753

--- torch/csrc/profiler/python/combined_traceback.cpp
+++ torch/csrc/profiler/python/combined_traceback.cpp
@@ -57,6 +57,15 @@ struct PythonTraceback : public CapturedTraceback::Python {
     py::str name_s = "name";
     py::str filename_s = "filename";
 
+    py::object stack_frames_for_code;
+    if (py::hasattr(torch, "_inductor")) {
+      py::object inductor = torch.attr("_inductor");
+      if (py::hasattr(inductor, "codecache")) {
+        stack_frames_for_code = inductor.attr("codecache")
+                                    .attr("PyCodeCache")
+                                    .attr("stack_frames_for_code");
+      }
+    }
     for (const auto& f : to_symbolize) {
       py::handle filename = f.code->co_filename;
       py::handle funcname = f.code->co_name;
@@ -67,6 +76,20 @@ struct PythonTraceback : public CapturedTraceback::Python {
           py::cast<std::string>(filename),
           py::cast<std::string>(funcname),
           (uint64_t)lineno});
+      // find all the additional frames associated with inductor generated
+      // code
+      if (stack_frames_for_code.ptr()) {
+        py::object extra = stack_frames_for_code(filename, lineno);
+        if (!extra.is_none()) {
+          for (py::handle h : extra) {
+            result.tracebacks.back().push_back(result.all_frames.size());
+            result.all_frames.emplace_back(unwind::Frame{
+                py::cast<std::string>(h[filename_s]),
+                py::cast<std::string>(h[name_s]),
+                py::cast<uint64_t>(h[line_s])});
+          }
+        }
+      }
     }
   }
 };
